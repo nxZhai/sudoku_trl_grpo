3f1b382928d906c8c89db95c2c36b2b7f38a3da3
nicy <oliverzhai1515@gmail.com>
2025-12-29 18:51:09 +0800

    fix typos in grpo_wen2.5-7b-it_lora-gpu.yaml

--- scripts/train_grpo_vllm_gpu.sh
+++ scripts/train_grpo_vllm_gpu.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=1,4,5,6
+export CUDA_VISIBLE_DEVICES=1,2,3,4
 
 accelerate launch \
     --num_processes 4 \
     --config_file ./configs/deepspeed_zero3.yaml \
     train_grpo.py \
-    --config ./configs/grpo_qwen2.5-3b-it_lora-gpu.yaml
+    --config ./configs/nicy_grpo_qwen2.5-7b-it_lora-gpu.yaml
